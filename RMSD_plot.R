d724c58bf3535929a640fb02bad6633866b4b346
ThomasCoMo <74672067+ThomasCoMo@users.noreply.github.com>
2021-07-29 09:29:42 +0200

    Update RMSD_plot.R

--- RMSD_plot.R
+++ RMSD_plot.R
@@ -1,7 +1,7 @@
-
+library('png')
 #set work directory
-nom<-'3b3k_7'
-setwd(paste('/home/tcoassin/Documents/Trajectoires/analyses/',nom,'/',sep = ''))
+name <- 'trajectory1'
+setwd(paste('path/to/directory/',name,'/',sep=''))
 
 #load RMSD data from auto.tcl scipt (vmd)
 data <- read.table('rmsd.dat')
@@ -14,20 +14,22 @@ name <- 'RMSD.png'
 dev.print(device = png, file = name, width=600)
 dev.off()
 
-setwd('/home/tcoassin/Documents/Trajectoires/analyses/')
 
-# parameter to merge previous picture
-par(mar=rep(0,4))
-nbrcol=4
-layout(matrix(1:7, ncol=nbrcol, byrow=TRUE))
-
-for (i in 1:7){
-  name = paste("./3b3k_",i,"/rmsd.png",sep="")
-  img <- readPNG(name)
-  plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
-  rasterImage(img,0,0,1,1)
-}
-# save in PDF
-output = paste("rmsd_3b3k",".pdf",sep="")
-dev.print(pdf, output)
-dev.off()
+## if you have multiple trajectory and you want one figure with all graphs decomment
+#setwd('path/to/your/analysis/')
+#
+## parameter to merge previous picture
+#par(mar=rep(0,4))
+#nbrcol=4
+#layout(matrix(1:7, ncol=nbrcol, byrow=TRUE))
+#
+##for (i in 1:7){
+#  name = paste("./trajectory",i,"/rmsd.png",sep="")
+#  img <- readPNG(name)
+#  plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
+#  rasterImage(img,0,0,1,1)
+#}
+## save in PDF
+#output = paste("rmsd_all",".pdf",sep="")
+#dev.print(pdf, output)
+#dev.off()
